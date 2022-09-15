0ae60dc374111c6f6af287784c6c757ecf9fc075
Tock <kaewsumran.p@gmail.com>
2022-09-15 12:24:05 +0700

    Database connect OK / OOP OK / Function query OK

--- functions/functions.php
+++ functions/functions.php
@@ -1,19 +1,22 @@
 <?php 
     include_once('config.php');
     
-    class DB_CON {
+    class DB_CON{
         function __construct()
         {
-            $conection = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME);
-            $this->dbcon=$conection;
-        }
-    
-    
+            $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME);
+            $this->conn=$connection;
+            if($connection->connect_errno){
+                echo "Not connect Database";
+            }
+     
 
-        public function sql($sql){
-            $result = mysqli_query($this->dbcon,$sql);
-            return $result;
         }
+        public function sql($sql){
+            $result = mysqli_query($this->conn,$sql);
+            return $result;
+            }
+        
     }
 
 ?>
